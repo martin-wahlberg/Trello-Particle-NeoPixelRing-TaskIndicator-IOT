9ee3e8743f162dc1102913dbee2e5a3310b54e17
Martin Wahlberg <wahmar16@student.westerdals.no>
2017-08-25 04:03:50 +0200

    Update particletrelloneopixelring.ino

--- particletrelloneopixelring.ino
+++ particletrelloneopixelring.ino
@@ -1,49 +1,48 @@
-#include "neopixel.h"
+// This #include statement was automatically added by the Particle IDE.
+#include <neopixel.h>
 
 
 SYSTEM_MODE(AUTOMATIC);
-#define PIXEL_COUNT 36
+
+#define PIXEL_COUNT 12
 #define PIXEL_PIN D6
 #define PIXEL_TYPE WS2812B
+
 Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
-
-
-int trellotall = 0;
+//Set timer for publishing event every 10 minutes
 Timer timer(600000, trelloTimer);
+int trellotall = 0;
 
-void setup() {
-Particle.function("TrelloComand", TrelloComand);
-strip.begin();
-timer.start();
-}
-
-void loop() {
-TrelloView();
-strip.show();
-}
-
-
-void trelloTimer()
-{
- Particle.publish("trello", "");
-}
-
-
-
-int TrelloComand(String command){
+    void setup(){
+    timer.start();
+    strip.begin();
+    }
+    
+    void loop(){
+    TrelloView();
+    strip.show();    
+    }
+    
+    //Publishing the event that triggers the webhook
+    void trelloTimer(){
+    Particle.publish("trello", "");
+    }
+    
+    int TrelloComand(String command){
+    //Converting to int
     int number = command.toInt();
     
     if(number != NULL){
-        //Minus one because of troubles passing 1.
+        //Minus one because of troubles passing 0.
         number = number - 1;
         trellotall = number;
         return 1;
     }
-}
-
-void TrelloView(){
+    }
+    
+    void TrelloView(){
   int lysstart;
-  lysstart = 0 + trellotall;
+  lysstart = trellotall;
   if(trellotall <= 0){
    trellotall = 0;      
    for (int i=0; i <= 11; i++){
